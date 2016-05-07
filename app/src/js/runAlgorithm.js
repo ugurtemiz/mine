a903838cbeba9c35c228a4e107c6096548f00414
Ugur Temiz <u.temiz@relephant.nl>
2016-05-07 18:23:25 +0300

    win32 detection

--- app/src/js/runAlgorithm.js
+++ app/src/js/runAlgorithm.js
@@ -1,13 +1,19 @@
 module.exports = function(options) {
-    var fs = require('fs');
     const exec = require('child_process').exec;
     var directory = './app/src/algorithms/fpgrowth';
     var result;
-    const child = exec(directory + '/src/fpgrowth -s' + options.s +
+    var command =   directory + '/src/fpgrowth -s' + options.s +
                                                 ' -m' + options.m +
                                                 ' -q' + options.q +
                                                 ' -b, ' + options.f + 
-                                                ' test1.csv',
+                                                ' test1.csv';
+
+    if (process.platform == 'win32') {
+        command = command.replace('src/fpgrowth', 'src/fpgrowth.exe');
+        command = command.split('/').join('\\');
+    }
+
+    const child = exec(command,
     (error, stdout, stderr) => {
         console.log(`stdout: ${stdout}`);
         console.log(`stderr: ${stderr}`);
